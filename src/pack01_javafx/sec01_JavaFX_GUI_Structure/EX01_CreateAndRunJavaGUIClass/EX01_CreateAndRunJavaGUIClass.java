e100532721d86f61865fca1632c8139fac26e0ea
kdh2465 <45729773+kdh2465@users.noreply.github.com>
2020-09-21 13:33:14 +0900

    주석일부 수정

--- src/pack01_javafx/sec01_JavaFX_GUI_Structure/EX01_CreateAndRunJavaGUIClass/EX01_CreateAndRunJavaGUIClass.java
+++ src/pack01_javafx/sec01_JavaFX_GUI_Structure/EX01_CreateAndRunJavaGUIClass/EX01_CreateAndRunJavaGUIClass.java
@@ -4,7 +4,7 @@ public class EX01_CreateAndRunJavaGUIClass {
 	public static void main(String[] args) {
 
 		//이렇게 별도로 구성할 수 있으나 반드시 Application 클래스는 public 이어야 함
-        //main 함수가 포함된 클래스가 직접 Application 클래스를 호출하는 경우 launch(args)로만 가능
+                //main 함수가 포함된 클래스가 직접 Application 클래스를 상속하는 경우 launch(args)로만 가능
 		MyStage.launch(MyStage.class, args);
 		
 	}
